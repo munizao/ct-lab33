d738cb52b0118be5c3d4452749bf6d4d9d8ce175
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-05 15:11:26 -0800

    added router

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import Home from './Home';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  // Switch,
+  // Link
+} from 'react-router-dom';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/details/:charId" component={Details} />
+    </Router>
+  );
 }
-  
