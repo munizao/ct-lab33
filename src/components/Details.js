299ad7a8aee02837590a8785c1f0e7da8f19bb54
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-06 09:12:38 -0800

    more details in Details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import useDetails from '../hooks/use-details';
+import Header from './Header';
+import '../style/common.css';
+import './Details.css';
+
+
 
 const Details = () => {
   const details = useDetails();
   return (
-    <dl>
-      <dt>Name</dt>
-      <dd>{details.name}</dd>
-    </dl>
+    <>
+      <Header subhead={'Character Details'}/>
+      <main>
+        <div>
+          <img src={details.photoUrl} />
+          <dl>
+            <dt>Name:</dt>
+            <dd>{details.name}</dd>
+            <dt>Position:</dt>
+            <dd>{details.position}</dd>
+            <dt>First Episode:</dt>
+            <dd>{details.first}</dd>
+          </dl>
+        </div>
+      </main>
+    </>
   );
 };
 
