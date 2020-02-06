cd8ec5c2cf2e71f5f2d93467afb5f6302d77ddba
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-05 17:07:45 -0800

    added paging

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import styles from './Home.css';
+import commonStyles from '../style/common.css'
 import useCharacters from '../hooks/use-characters';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const { characters,  } = useCharacters();
+  const { characters, page, prevPage, nextPage } = useCharacters();
 
   return (
-    <div>
-      <p>Character List:</p>
-      <ul>
-        {characters.map((character) => <li key={character._id}><Link to={`/details/${character._id}`}>{character.name}</Link></li>)}
-      </ul>
-    </div>
+    <>
+      <header><h1>Character List:</h1></header>
+      <main>
+        <div className={styles.Main}>
+          <ul>
+            {characters.map((character) => <li key={character._id}><Link to={`/details/${character._id}`}>{character.name}</Link></li>)}
+          </ul>
+          <span onClick={prevPage} className={commonStyles.Buttonish}>Previous</span><span onClick={nextPage} className={commonStyles.Buttonish}>next</span>
+        </div>
+      </main>
+    </>
   );
 };
 
